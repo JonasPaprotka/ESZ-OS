48cdc94ecd92d99813e01ec111f21a9c3572066e
Paprotka Jonas <paprotka.jonas@gmail.com>
2026-06-13 17:33:36 +0200

    added sleep_ms and sleep_sec

--- kernel/io/timer/timer.cpp
+++ kernel/io/timer/timer.cpp
@@ -10,6 +10,20 @@ extern "C" void timer_handler() {
     EIO_Finished_Interrupt();
 }
 
-uint64_t get_ticks_in_ms() {
+const uint64_t get_ticks_in_ms() {
     return ticks * 1000 / PIT_TICK_HZ;
+}
+
+void halt_till_tick(const uint64_t targetTick) {
+    while(ticks < targetTick) {
+        __asm__ volatile("sti; hlt");
+    }
+}
+
+void sleep_sec(const uint64_t sec) {
+    halt_till_tick(ticks + PIT_TICK_HZ * sec);
+}
+
+void sleep_ms(const uint64_t ms) {
+    halt_till_tick(ticks + ms * PIT_TICK_HZ / 1000);
 }
--- kernel/io/timer/timer.h
+++ kernel/io/timer/timer.h
@@ -7,6 +7,9 @@ extern volatile uint64_t ticks;
 
 extern "C" void timer_handler();
 
-uint64_t get_ticks_in_ms();
+const uint64_t get_ticks_in_ms();
+
+void sleep_sec(const uint64_t sec);
+void sleep_ms(const uint64_t ms);
 
 #endif
