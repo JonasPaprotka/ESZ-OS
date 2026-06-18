07ba31240609243d99e71837c443fa3e82b41c81
Paprotka Jonas <paprotka.jonas@gmail.com>
2026-06-18 20:04:55 +0200

    added PCI structs

--- kernel/io/pci/pci.h
+++ kernel/io/pci/pci.h
@@ -1,6 +1,57 @@
 #ifndef PCI_H
 #define PCI_H
 
+#include <stdint.h>
 
+struct PCI_Memory_Space_BAR_MMIO {
+    uint32_t RegionType : 1; // 1 bit
+    uint32_t MemoryType : 2; // 2 bits
+    uint32_t Prefetchable : 1; // 1 bit
+    uint32_t BaseAddress : 28; // 28 bits
+} __attribute__((packed));
+
+struct PCI_IO_Space_BAR {
+    uint32_t RegionType : 1; // 1 bit
+    uint32_t Reserved : 1; // 1 bit
+    uint32_t BaseAddress : 30; // 30 bits
+} __attribute__((packed));
+
+union PCI_BAR {
+    uint32_t raw;
+    PCI_Memory_Space_BAR_MMIO mem;
+    PCI_IO_Space_BAR io;
+} __attribute__((packed));
+
+struct PCI_Device {
+    uint16_t VendorID;
+    uint16_t DeviceID;
+    uint16_t Command;
+    uint16_t Status;
+
+    uint8_t  RevisionID;
+    uint8_t  ProgIF;
+    uint8_t  Subclass;
+    uint8_t  ClassCode;
+
+    uint8_t  CacheLineSize;
+    uint8_t  LatencyTimer;
+    uint8_t  HeaderType;
+    uint8_t  BIST;
+
+    PCI_BAR BAR[6]; // 6*32 bits
+
+    uint32_t CardbusCISPointer;
+    uint16_t SubsystemVendorID;
+    uint16_t SubsystemID;
+    uint32_t ExpansionROMBaseAddress;
+
+    uint8_t  CapabilitiesPointer;
+    uint8_t  Reserved[7];
+
+    uint8_t  InterruptLine;
+    uint8_t  InterruptPin;
+    uint8_t  MinGnt;
+    uint8_t  MaxLat;
+} __attribute__((packed));
 
 #endif
