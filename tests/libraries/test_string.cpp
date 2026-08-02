ea0226d9514ad15597418e8f8393ee6b634b8f0f
Paprotka Jonas <paprotka.jonas@gmail.com>
2026-08-02 13:40:03 +0200

    str_equal tests

--- tests/libraries/test_string.cpp
+++ tests/libraries/test_string.cpp
@@ -9,3 +9,12 @@ TEST_CASE("str_length") {
     CHECK(str_length("ESZ-OS") == 6);
     CHECK(str_length("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l") == 100);
 }
+
+TEST_CASE("str_equal") {
+    CHECK(str_equal("A", "A") == true);
+    CHECK(str_equal("A", "B") == false);
+    CHECK(str_equal("A", "a") == false);
+    CHECK(str_equal("Lorem ipsum dolor", "Lorem ipsum dolor") == true);
+    CHECK(str_equal(" ", " ") == true);
+    CHECK(str_equal("", "") == true);
+}
