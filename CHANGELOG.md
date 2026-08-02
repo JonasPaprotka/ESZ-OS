f6a389a8d3bff0e15f55ac2467ff62964226981f
Paprotka Jonas <paprotka.jonas@gmail.com>
2026-08-02 16:40:33 +0200

    init new version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.3.2
+
+
+
 ## v0.3.1
 
 - Repository
--- kernel/config.h
+++ kernel/config.h
@@ -24,7 +24,7 @@
 // --- VERSION TRACKER ---
 #define OS_VERSION_MAJOR 0
 #define OS_VERSION_MINOR 3
-#define OS_VERSION_PATCH 1
+#define OS_VERSION_PATCH 2
 // -----------------------
 
 // --- TERMINAL CONFIG ---
