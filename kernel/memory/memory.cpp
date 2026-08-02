9638e469ef4a9a93b6e0b58d3ceb2265a097a242
Jonas Paprotka <paprotka.jonas@gmail.com>
2026-08-02 16:02:47 +0200

    Fix bit_* functions taking int instead of uint64_t (#36)

--- kernel/memory/memory.cpp
+++ kernel/memory/memory.cpp
@@ -58,12 +58,14 @@ void init_bitmap_data() {
 void get_free_location_for_bitmap() {
     pageCount = highestAddress / PAGE_SIZE;
     uint64_t reqSize = divide_round_up(pageCount, 8);
+    uint64_t bitmapPhysBase = 0;
 
     for (uint64_t i = 0; i < memoryRegionCount; i++) {
         struct limine_memmap_entry* entry = memmap_request.response->entries[i];
 
         if (entry->type == LIMINE_MEMMAP_USABLE) {
             if (entry->length >= reqSize) {
+                bitmapPhysBase = entry->base;
                 pmm_bitmap = (unsigned char*) entry->base + hhdm_offset;
                 break;
             }
@@ -73,7 +75,7 @@ void get_free_location_for_bitmap() {
     memory_fill(pmm_bitmap, 0xFF, reqSize); // mark everything as filled
     init_bitmap_data();
 
-    uint64_t bitmapStartPage = (uint64_t)pmm_bitmap / PAGE_SIZE;
+    uint64_t bitmapStartPage = bitmapPhysBase / PAGE_SIZE;
     uint64_t bitmapPagesCount = divide_round_up(reqSize, PAGE_SIZE);
     pmm_malloc_page_range(bitmapStartPage, bitmapPagesCount);
 }
--- kernel/types/bit/bit.cpp
+++ kernel/types/bit/bit.cpp
@@ -1,17 +1,18 @@
+#include <stdint.h>
 
-void bit_set(unsigned char* bitmap, const int page) {
+void bit_set(unsigned char* bitmap, const uint64_t page) {
     bitmap[page / 8] |= (1 << (page % 8));
 }
 
-void bit_clear(unsigned char* bitmap, const int page) {
+void bit_clear(unsigned char* bitmap, const uint64_t page) {
     bitmap[page / 8] &= ~(1 << (page % 8));
 }
 
-void bit_write(unsigned char* bitmap, const int page, const bool value) {
+void bit_write(unsigned char* bitmap, const uint64_t page, const bool value) {
     if (value) bit_set(bitmap, page);
     else bit_clear(bitmap, page);
 }
 
-bool bit_read(unsigned char* bitmap, const int page) {
+bool bit_read(unsigned char* bitmap, const uint64_t page) {
     return (bitmap[page / 8] >> (page % 8)) & 1;
 }
